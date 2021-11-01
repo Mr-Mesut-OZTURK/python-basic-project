359f318073db67bfe4d18c35ed05fc650a0fea0e
Mesut ÖZTÜRK <mesut8311006@gmail.com>
2021-11-01 20:37:50 +0300

    sms | added password page

---
+++ school-management-system/document/password.py
@@ -0,0 +1,7 @@
+paswords = [
+    {"title" : "director", "name" : "mesut", "password" : "1234"},
+    {"title" : "teacher", "name" : "ibrahim", "password" : "1234"},
+    {"title" : "student", "name" : "yakup", "password" : "1234"},
+    {"title" : "student", "name" : "ahmet", "password" : "1234"},
+    {"title" : "student", "name" : "mehmet", "password" : "1234"}
+] 
